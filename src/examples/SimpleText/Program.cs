fa526b2f383cc54d56b13972ee2c8cb2b7dbd058
Sebastian Gingter <sebastian@gingter.org>
2020-03-29 18:56:41 +0100

    Added initial docs

--- src/examples/SimpleText/Program.cs
+++ src/examples/SimpleText/Program.cs
@@ -9,14 +9,13 @@ namespace SimpleText
 	{
 		public static int Main(string[] args)
 		{
-			BdfFont.ExtractFontFiles("./fonts");
-
 			var matrix = new LedMatrix(new LedMatrixOptions(), args);
 			var canvas = matrix.CreateOffscreenCanvas();
+			canvas.DrawLine(0, 0, 16, 16, new Color(255, 0, 0));
 
-			var font = new BdfFont("./fonts/6x13.bdf");
-
+			var font = new BdfFont("6x13.bdf");
 			canvas.DrawText(font, 1, 20, new Color(0, 255, 0), "Thank you!");
+
 			matrix.SwapOnVsync(canvas);
 
 			while (!Console.KeyAvailable)
